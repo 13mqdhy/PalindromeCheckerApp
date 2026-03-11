648d51c55161b8a4ce119745253277d6f6b8a51d
13mqdhy <l.madheshwaran@gmail.com>
2026-03-11 23:08:02 +0530

    UC6

--- App/src/PalindromeCheckerApp.java
+++ App/src/PalindromeCheckerApp.java
@@ -1,21 +1,41 @@
-import java.util.Scanner;
+import java.util.Queue;
+import java.util.LinkedList;
+import java.util.Stack;
 
 public class PalindromeCheckerApp {
-    public static void main(String args[]){
+    public static void main(String[] args) {
 
-        Scanner scanner = new Scanner(System.in);
-        System.out.print("Enter a string to check if it is a palindrome: ");
-        String originalString = scanner.nextLine();
-        String reversedString = "";
-        for (int i = originalString.length() - 1; i >= 0; i--) {
-            reversedString = reversedString + originalString.charAt(i);
-        }
-        if (originalString.equals(reversedString)) {
-            System.out.println("The given string is a Palindrome.");
-        } else {
-            System.out.println("The given string is NOT a Palindrome.");
-        }
 
-        scanner.close();
+        String input = "civic";
+
+        Queue<Character> queue = new LinkedList<>();
+        Stack<Character> stack = new Stack<>();
+
+
+        for (char c : input.toCharArray()) {
+            queue.add(c);
+            stack.push(c);
+
+            Scanner scanner = new Scanner(System.in);
+            System.out.print("Enter a string to check if it is a palindrome: ");
+            String originalString = scanner.nextLine();
+            String reversedString = "";
+            for (int i = originalString.length() - 1; i >= 0; i--) {
+                reversedString = reversedString + originalString.charAt(i);
+
+            }
+
+            boolean isPalindrome = true;
+
+
+            while (!queue.isEmpty()) {
+                if (queue.remove() != stack.pop()) {
+                    isPalindrome = false;
+                    break;
+                }
+            }
+
+            System.out.println("Input: " + input);
+            System.out.println("Is Palindrome? " + isPalindrome);
+        }
     }
-}
