89f8244cc15a3bb36b6f689c9f6e59ec63073f75
madheshhhhh420 <72644064+13mqdhy@users.noreply.github.com>
2026-03-11 21:58:35 +0530

    Delete UC1.java

--- UC1.java
+++
@@ -1,7 +0,0 @@
-public class UC1 {
-    public static void main(String args[]){
-        System.out.println("Welcome to Palindrome Checker Management System");
-        System.out.println("version ; 1.0");
-        System.out.println("System initialization successfully");
-    }
-}
