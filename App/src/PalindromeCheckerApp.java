431cbab06396b239250ccd2e3e03a4ad54c6f93d
13mqdhy <l.madheshwaran@gmail.com>
2026-03-11 23:03:59 +0530

    UC5

--- App/src/PalindromeCheckerApp.java
+++ App/src/PalindromeCheckerApp.java
@@ -1,10 +1,7 @@
-
 import java.util.Scanner;
 
 public class PalindromeCheckerApp {
     public static void main(String args[]){
-
-
 
         Scanner scanner = new Scanner(System.in);
         System.out.print("Enter a string to check if it is a palindrome: ");
@@ -20,24 +17,5 @@ public class PalindromeCheckerApp {
         }
 
         scanner.close();
-
     }
 }
-
-
-public class PalindromeCheckerApp {
-    public static void main(String args[]){
-        String str="madam";
-        boolean ispalindrome=true;
-        for(int i=0; i<str.length()/2; i++){
-            if(str.charAt(i) != str.charAt(str.length() -1 -i)){
-                ispalindrome = false;
-                break;
-            }
-        }
-        System.out.println("Input text: "+str);
-        System.out.println("\n Is it a palindrome : True");
-
-    }
-}
-
